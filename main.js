85b53d45ab944a4351fb10648f6366ddf5f0975b
Camilo Jimenez <camiloko1989@gmail.com>
2022-12-20 20:00:44 -0800

    Se añade aside con Detalles de Productos y se corrigen estilos de CSS

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarrito = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardContainer = document.querySelector('.cards-container');
 
 
@@ -13,20 +13,20 @@ menuCarrito.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
 
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if(!isAsideClosed){ //valida si el menu mobile está abiert
-        aside.classList.add('inactive') //cierra el menu mobile
+        shoppingCartContainer.classList.add('inactive') //cierra el menu mobile
     }
 
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if(!isAsideClosed){ //valida si el menu mobile está abiert
-        aside.classList.add('inactive') //cierra el menu mobile
+        shoppingCartContainer.classList.add('inactive') //cierra el menu mobile
     }
 
     mobileMenu.classList.toggle('inactive');
@@ -41,7 +41,7 @@ function toggleCarritoAside(){
         mobileMenu.classList.add('inactive') //cierra el menu mobile
     }
 
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 
 }
 
